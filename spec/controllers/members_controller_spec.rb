09b3e59de548b088b2243d325723ed1586f13018
Willian Lucio <willian.l.lucio@gmail.com>
2017-11-17 16:20:17 -0200

    adjust test member controler in action create

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe MembersController, type: :controller do
   include Devise::Test::ControllerHelpers
 
   before(:each) do
+    request.env["HTTP_ACCEPT"] = 'application/json'
     @request.env["devise.mapping"] = Devise.mappings[:user]
     @current_user = FactoryGirl.create(:user)
     sign_in @current_user
@@ -13,38 +14,41 @@ RSpec.describe MembersController, type: :controller do
     
     context "the user owns the campaign" do
       before(:each) do
-        request.env["HTTP_ACCEPT"] = 'application/json'
-
         @campaign = create(:campaign, user: @current_user)
-        @member_attributes = attributes_for(:member, campaign: @campaign)
-        post :create, params: {member: @member_attributes, id: @campaign}
+        @member = build(:member, campaign: @campaign)
+
+        post :create, params: {member: @member.attributes}
       end
 
-      it "created member with correct data" do
-        expect(Member.last.name).to eql(@member_attributes.name)     
+      it "created member with correct name" do
+        expect(Member.last.name).to eql(@member.name)   
       end
 
-      # it "created member was associated with the correct campaign" do
-      #   expect(Member.last.campaign).to eql(@campaign)
-      # end
+      it "created member with correct email" do
+        expect(Member.last.email).to eql(@member.email)   
+      end
 
-      # it "return the status 200 when it successful" do
-      #   expect(response).to have_http_status(:success)
-      # end
+      it "created member was associated with the correct campaign" do
+        expect(Member.last.campaign).to eql(@campaign)
+      end
+
+      it "return the status 200 when it successful" do
+        expect(response).to have_http_status(:success)
+      end
   
-      # # Leo, este é o teste que não está passando. Deveria retornar 422 mas retorna 200
       # it "return the status 422 when member is already present" do
-      #   @campaign.members << @member
-      #   # expect(response).to have_http_status(422)
+      #   post :create, params: {member: @member.attributes}
+      #   expect(response).to have_http_status(:unprocessable_entity)
       # end
     end
 
-    # # Leo, este é o teste que não está passando. Deveria retornar 403 mas retorna 200
-    # it "the user not owns campaign" do
-    #   @campaign = create(:campaign)
-    #   @campaign.members << create(:member)
-    #   expect(response).to have_http_status(403)
-    # end
+    it "the user not owns campaign" do
+      @campaign = create(:campaign)
+      @member = build(:member, campaign: @campaign)
+      post :create, params: {member: @member.attributes}
+      
+      expect(response).to have_http_status(403)
+    end
   end
 
   describe "GET #destroy" do
